264825511d21da5125cffcba300b458575c366f2
MahinGugu <mahinthan.gugananthan@gmail.com>
2024-06-16 01:19:08 +0200

    Created : Category Entity and Product Entity with EasyAdmin + mapping of both

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Produit')
+            ->setEntityLabelInPlural('Produits')
+        ;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('Nom')->setHelp('Nom de votre produit'),
+            SlugField::new('slug')->setTargetFieldName('name')->setLabel('URL')->setHelp("URL de la catégorie générée automatiquement"),
+            TextEditorField::new('description')->setLabel('Description')->setHelp('Description de votre produit'),
+            ImageField::new('image')->setLabel('Image')->setHelp('Image du produit en 600x600 pixels')->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')->setBasePath('/uploads')->setUploadDir('public\uploads'),
+            NumberField::new('price')->setLabel('Prix HT')->setHelp('Prix HT de votre produit, sans le sigle €'),
+            ChoiceField::new('tva')->setLabel('Taux de TVA')->setChoices([
+                '5.5%' => '5.5',
+                '10%' => '10',
+                '20%' => '20'
+            ]),
+            AssociationField::new('category')->setLabel('Catégorie associée'),
+
+
+        ];
+    }
+    
+}
